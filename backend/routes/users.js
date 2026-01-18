b29737f4792090be69df3112ba38ccc6ea3afa59
paullara <zaykinn66@gmail.com>
2026-01-18 10:52:13 +0800

    fixed table

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -18,6 +18,15 @@ router.post("/", (req, res) => {
   });
 });
 
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  db.query("UPDATE user SET name = ? WHERE id =?", [name, id], (err) => {
+    if (err) return res.status(500).json(err);
+    return res.json({ message: "User updated" });
+  });
+});
+
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
   db.query("DELETE FROM user WHERE id = ?", [id], (err) => {
